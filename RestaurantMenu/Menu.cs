72d5cf2489fff644f382fffd5f769d9d9e0775f1
Sabeena <sabeenarayyan13@gmail.com>
2021-08-18 19:43:07 +0530

    Add project files.

---
+++ RestaurantMenu/Menu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantMenu
+{
+    public class Menu
+    {
+        public DateTime LastUpdated { get; set; }
+        public List<MenuItem> Items { get; set; }
+
+        public Menu(DateTime d, List<MenuItem> i)
+        {
+            LastUpdated = d;
+            Items = i;
+            
+        }
+    }
+}
---
+++ RestaurantMenu/MenuItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantMenu
+{
+    public class MenuItem
+    {
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public bool IsNewItemAdded { get; set; }
+
+        public MenuItem(double price,string description,string category,bool isnewItemAdded)
+        {
+            Price = price;
+            Description = description;
+            Category = category;
+            IsNewItemAdded = isnewItemAdded;
+        }
+
+    }
+}
